5670d628085e8ae466f2895a6fef08d8f18a15a2
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-28 19:43:07 +0100

    Stric action types with useReducer

--- src/components/state/Counter.tsx
+++ src/components/state/Counter.tsx
@@ -4,17 +4,24 @@ type StateProps = {
   count: number
 }
 
-type ActionProps = {
-  type: string,
+type UpdateAction = {
+  type: 'increment' | 'decrement',
   payload: number
 }
 
+type ResetAction = {
+  type: 'reset',
+}
+
+type CounterAction = UpdateAction | ResetAction
+
 const initialState = {count: 0}
 
-const reducer = (state: StateProps, action: ActionProps) => {
+const reducer = (state: StateProps, action: CounterAction) => {
   switch(action.type) {
-    case 'increment': return {count: state.count + action.payload}
-    case 'decrement': return {count: state.count - action.payload}
+    case 'increment': return { count: state.count + action.payload }
+    case 'decrement': return { count: state.count - action.payload }
+    case 'reset': return initialState
     default: return state
   }
 }
@@ -26,7 +33,8 @@ const Counter = () => {
         Count: {state.count}
 
         <button onClick={() => dispatch({ type: 'increment', payload: 10 })}>Increment</button>
-        <button onClick={() => dispatch({ type: 'decrement', payload: 10 })}>DEcrement</button>
+        <button onClick={() => dispatch({ type: 'decrement', payload: 10 })}>Decrement</button>
+        <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
     </div>
   )
 }
