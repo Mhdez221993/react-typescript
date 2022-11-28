e8b1a27929938061a7078ac7e2fe73f953a8b8dc
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-29 00:51:12 +0100

    Polymorphic component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import './App.css';
 
-import CustomeComponent from './components/html/CustomeComponent';
+import Text from './components/polymorphic/Text';
 
 function App() {
   return (
     <div className="App">
-     <CustomeComponent name='Doe' isLoggedIn={true} />
+     <Text as='h1' color='primary' size='lg'>
+        Heading
+     </Text>
+
+     <Text as='p' color='secondary' size='sm'>
+        Paragraph
+     </Text>
+
+     <Text as='p' htmlFor='someId' color='secondary' size='sm'>
+        Label
+     </Text>
     </div>
   )
 }
---
+++ src/components/polymorphic/Text.tsx
@@ -0,0 +1,21 @@
+
+type TextOwnProps<E extends React.ElementType> = {
+  size?: 'sm' | 'lg' | 'md'
+  color?: 'primary' | 'secondary'
+  children: React.ReactNode
+  as: E
+}
+
+type TextProps<E extends React.ElementType> = TextOwnProps<E> & Omit<React.ComponentProps<E>, keyof TextOwnProps<E>>
+
+const Text = <E extends React.ElementType = 'div'>({size, color, children, as}: TextProps<E>) => {
+  const Component = as || 'div'
+
+  return (
+    <Component className={`class-wit-${size}-${color}`}>
+      {children}
+    </Component>
+  )
+}
+
+export default Text
