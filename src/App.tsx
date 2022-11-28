6796bdf2cd548f808e2d145d104dd98b97d76bd5
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-28 08:26:59 +0100

    Typing props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
 
+import Greet from './components/Greet';
+
 function App() {
   return (
     <div className="App">
-
+      <Greet name="Doe" />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
