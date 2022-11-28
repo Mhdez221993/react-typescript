b14bacf2300c3ac0713d6fbb0b79711e0d17600b
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-28 17:15:34 +0100

    Type event on handleEvent method

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 
 import Button from './components/Button';
+import Input from './components/Input';
 
 function App() {
   return (
     <div className="App">
       <Button handleClick={(e, id) => console.log('Button clicked', e, id)}/>
+      <Input value='' handleChange={(e) => console.log(e)} />
     </div>
   )
 }
